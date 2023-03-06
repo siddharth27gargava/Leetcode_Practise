22decc37ead0ce8c4c80d1e43ea5b4eea080f681
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2023-03-06 18:32:52 -0500

    Time: 0 ms (100.00%), Space: 41.9 MB (56.67%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,45 +1,44 @@
 class Solution {
     public int search(int[] nums, int target) {
-         if (nums.length == 0 || nums == null){
-            return -1;
-        }
-        //Find pivot point
+        
+        //find pivot
         int left = 0;
         int right = nums.length - 1;
-        while (right > left){
-            int mp = left + (right - left)/2;
-            if (nums[mp] > nums[right]){
+        
+        while(left < right){
+            int mp = (left+right)/2;
+            
+            if(nums[mp] > nums[right]){
                 left = mp + 1;
             } else {
                 right = mp;
             }
         }
-
+        
+        
+        //find the array to search in
         int start = left;
         left = 0;
         right = nums.length - 1;
-
-        //Find which sub-array to search in
-            if (nums[start] <= target && nums[right] >= target){
-                left = start;
-            } else {
-                right = start;
-            }
+        if(nums[start] <= target && nums[right] >= target){
+            left = start;
+        } else {
+            right = start;
+        }
         
-        
-        //Normal binary search
-        while (left <= right){
-            int hp = left + (right - left)/2;
-
-            if (nums[hp] > target){
-                right = hp - 1;
-            } else if(nums[hp] < target){
-                left = hp + 1;
+        //binary search
+        while(left <= right){
+            int mp = (left + right)/2;
+            
+            if(nums[mp] > target){
+                right = mp-1;
+            } else if(nums[mp] < target){
+                left = mp+1;
             } else {
-                return hp;
+                return mp;
             }
         }
-
+        
         return -1;
     }
 }
