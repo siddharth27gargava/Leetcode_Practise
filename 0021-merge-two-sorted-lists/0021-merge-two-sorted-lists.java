a774050441d8e21baf4c528fa36618bb8c57a7ad
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2023-03-06 12:47:24 -0500

    Time: 0 ms (100.00%), Space: 42 MB (38.05%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -17,15 +17,15 @@ class Solution {
             if(list1.val > list2.val){
                 tail.next = list2;
                 list2 = list2.next;
-            } else {
+                tail = tail.next;
+            } else{
                 tail.next = list1;
                 list1 = list1.next;
+                tail = tail.next;
             }
-            
-            tail = tail.next;
         }
         
-        if(list1 == null){
+        if(list1==null){
             tail.next = list2;
         } else if(list2 == null){
             tail.next = list1;
