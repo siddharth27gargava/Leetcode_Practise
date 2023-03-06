3303c2c8d9ddfe111d639189adc325eff28217c6
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2023-03-06 02:20:34 -0500

    Time: 10 ms (31.76%), Space: 46.7 MB (90.65%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,33 @@
+class Solution {
+    
+    public static class Pair implements Comparable<Pair>{
+        int x;
+        int y;
+        
+        public Pair(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+        
+        @Override
+        public int compareTo(Pair o2){
+            return this.x - o2.x;
+        }
+    }
+    
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b) -> Integer.compare(a[0],b[0]));
+        
+        LinkedList<int[]> ll = new LinkedList<>();
+        
+        for(int[] interval : intervals){
+            if(ll.isEmpty() || ll.getLast()[1] < interval[0]){
+                ll.add(interval);
+            } else {
+                ll.getLast()[1] = Math.max(ll.getLast()[1],interval[1]);
+            }
+        }
+        
+        return ll.toArray(new int[ll.size()][2]);
+    }
+}
