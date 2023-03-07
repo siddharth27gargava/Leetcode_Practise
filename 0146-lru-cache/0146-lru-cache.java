dd1ce074bef6ea0d71b0b30a9dbaf070c6ace98d
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2023-03-06 19:06:26 -0500

    Time: 62 ms (69.46%), Space: 111.3 MB (75.30%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,5 +1,6 @@
 class LRUCache {
-    class Node{
+    
+    public class Node{
         int key;
         int val;
         Node next;
@@ -8,67 +9,66 @@ class LRUCache {
     
     final Node head = new Node();
     final Node tail = new Node();
-    HashMap<Integer,Node> nodemap;
-    int cache_capacity;
+    HashMap<Integer, Node> map;
+    int capacity;
     
-    
-
     public LRUCache(int capacity) {
-        nodemap = new HashMap<>();
-        this.cache_capacity = capacity;
         head.next = tail;
         tail.prev = head;
+        map = new HashMap<>();
+        this.capacity = capacity;
+    }
+    
+    public void remove(Node node){
+        Node node_prev = node.prev;
+        Node node_next = node.next;
+        
+        node_prev.next = node_next;
+        node_next.prev = node_prev;
     }
     
     public void add(Node node){
         Node head_next = head.next;
-
+        
         node.next = head_next;
         head_next.prev = node;
-
+        
         head.next = node;
         node.prev = head;
-
-    }
-
-    public void remove(Node node){
-        Node next_node = node.next;
-        Node prev_node = node.prev;
-
-        prev_node.next = next_node;
-        next_node.prev = prev_node;
     }
     
     public int get(int key) {
         int result = -1;
-        Node node = nodemap.get(key);
-        if (node!=null){
-            remove(node);
-            result = node.val;
-            add(node);
+        
+        Node nn = map.get(key);
+        
+        if(nn!=null){
+            remove(nn);
+            result = nn.val;
+            add(nn);
         }
-
+        
         return result;
     }
     
     public void put(int key, int value) {
-        Node node = nodemap.get(key);
-        if (node!=null){
+        Node node = map.get(key);
+        
+        if(node!=null){
             remove(node);
             node.val = value;
             add(node);
         } else {
-            if (nodemap.size() >= cache_capacity){
-                nodemap.remove(tail.prev.key);
+            if(map.size() >= capacity){
+                map.remove(tail.prev.key);
                 remove(tail.prev);
             }
-
+            
             Node nn = new Node();
             nn.key = key;
             nn.val = value;
-
-            nodemap.put(key,nn);
             add(nn);
+            map.put(key,nn);
         }
     }
 }
