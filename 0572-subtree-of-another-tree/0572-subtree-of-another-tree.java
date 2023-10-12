73453dbc4b597216d88ef87b501355e980b375ad
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2023-10-12 02:46:28 -0400

    Time: 2 ms (96.82%), Space: 42.7 MB (92.12%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -23,23 +23,22 @@ class Solution {
             return false;
         }
         
-        if(sameTree(root,subRoot)){
+        //check if they are same tree
+        if(isSameTree(root,subRoot)){
             return true;
         }
         
-        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot); 
     }
     
-    public boolean sameTree(TreeNode root, TreeNode subRoot){
-        if(root == null && subRoot == null){
+    public boolean isSameTree(TreeNode p, TreeNode q){
+        if(p == null && q == null){
             return true;
         }
         
-        if(root != null && subRoot != null && root.val == subRoot.val){
-            return (sameTree(root.left,subRoot.left) &&
-             sameTree(root.right,subRoot.right));
+        if(p!=null && q!=null && p.val==q.val){
+            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
         }
-        
         
         return false;
     }
