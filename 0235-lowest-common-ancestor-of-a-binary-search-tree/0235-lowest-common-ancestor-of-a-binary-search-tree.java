5d2186745cab3767d150d9376bffa08d387faf35
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2023-10-13 23:09:33 -0400

    Time: 5 ms (100.00%), Space: 43.9 MB (43.45%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -14,10 +14,10 @@ class Solution {
             return null;
         }
         
-        if(root.val > p.val && root.val > q.val){
-            return lowestCommonAncestor(root.left,p,q);
-        } else if(root.val < p.val && root.val < q.val){
+        if(p.val > root.val && q.val > root.val){
             return lowestCommonAncestor(root.right,p,q);
+        } else if(p.val < root.val && q.val < root.val){
+            return lowestCommonAncestor(root.left,p,q);
         } else {
             return root;
         }
