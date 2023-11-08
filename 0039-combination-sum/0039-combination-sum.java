1085d23e19e6d8be9002a5b9057e97331b8708dd
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2023-11-08 16:19:18 -0500

    Time: 3 ms (30.14%), Space: 43.5 MB (88.21%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,30 +1,37 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        if (candidates.length == 0){
-            return null;
-        }
-
-        List<List<Integer>> ans = new ArrayList<>();
-
-        findCombination(candidates,0, target, new ArrayList<>(), ans);
-
-        return ans;
+        Arrays.sort(candidates);
+        
+        List<List<Integer>> res = new ArrayList<>();
+        
+        helper(0,candidates,target,res,new ArrayList<>());
+        
+        return res;
+        
     }
     
-    public void findCombination(int[] candidates, int idx, int target, List<Integer> curr, List<List<Integer>> res){
+    public void helper(int idx, int[] arr, int target, List<List<Integer>> res, List<Integer> curr){
+        
+        //Base Case-1
         if(target == 0){
             res.add(new ArrayList<>(curr));
             return;
         }
         
-        
-        for(int i = idx; i < candidates.length; i++){
-            if(candidates[i] <= target){
-                curr.add(candidates[i]);
-                findCombination(candidates, i, target - candidates[i], curr, res);
-                curr.remove(curr.size() - 1);
-            }
+        //Base Case-2
+        if(idx >= arr.length || target < 0){
+            return;
         }
+        
+        //include
+        curr.add(arr[idx]);
+        helper(idx,arr,target - arr[idx],res,curr);
+        
+        //exclude - don't want to include arr[i] ever again
+        curr.remove(curr.size() - 1);
+        helper(idx + 1,arr,target,res,curr);
+        
     }
+    
     
 }
