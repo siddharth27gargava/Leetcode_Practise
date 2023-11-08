e16fe800118e0cd23ee72af03adabe736053337d
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2023-11-08 18:59:16 -0500

    Time: 1 ms (99.90%), Space: 43.8 MB (40.65%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,26 +1,32 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        generate(0, nums, result, new ArrayList());
-        return result;
+        
+        List<List<Integer>> res = new ArrayList<>();
+        
+        helper(0, nums, res, new ArrayList<>());
+        
+        return res;
     }
     
-    public void generate(int idx, int[] nums, List<List<Integer>> res, List<Integer> subs){
-       if(idx == nums.length){
-           res.add(new ArrayList(subs));
-           return;
-       }
+    public void helper(int idx, int[] nums, List<List<Integer>> res, List<Integer> curr){
+        //Base Case
+        if(idx == nums.length){
+            res.add(new ArrayList<>(curr));
+            return;
+        }
         
-        //All subsets which include nums[i]
-        subs.add(nums[idx]);
-        generate(idx + 1, nums, res, subs);
-        subs.remove(subs.size()-1);
+        //Include
+        curr.add(nums[idx]);
+        helper(idx + 1, nums, res, curr);
         
-        //All subsets which don't include nums[i]
-        while(idx + 1 < nums.length && nums[idx] == nums[idx+1]){
+        //Duplicate remove
+        while(idx + 1 < nums.length && nums[idx] == nums[idx + 1]){
             idx++;
         }
-        generate(idx + 1, nums, res, subs);
+        
+        //Exclude
+        curr.remove(curr.size() - 1);
+        helper(idx + 1, nums, res, curr);
     }
 }
