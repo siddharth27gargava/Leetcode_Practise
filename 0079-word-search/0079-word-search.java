90a429dd72afa0d4dd86ac182abd37c169f72892
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2023-11-16 16:23:23 -0500

    Time: 156 ms (44.95%), Space: 40.7 MB (36.17%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,9 +1,8 @@
 class Solution {
     
-    boolean visited[][];
+    boolean[][] visited;
     
     public boolean exist(char[][] board, String word) {
-        
         visited = new boolean[board.length][board[0].length];
         
         for(int i = 0; i < board.length; i++){
@@ -22,16 +21,16 @@ class Solution {
             return true;
         }
         
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || word.charAt(idx)!=board[i][j]){
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || word.charAt(idx) != board[i][j]){
             return false;
         }
         
         visited[i][j] = true;
         
-        if(dfs(i+1,j,idx+1,visited,word,board) || 
-           dfs(i-1,j,idx+1,visited,word,board) || 
-           dfs(i,j+1,idx+1,visited,word,board) || 
-           dfs(i,j-1,idx+1,visited,word,board)){
+        if(dfs(i+1,j,idx+1,visited,word,board) ||
+          dfs(i-1,j,idx+1,visited,word,board) ||
+          dfs(i,j+1,idx+1,visited,word,board) ||
+          dfs(i,j-1,idx+1,visited,word,board)){
             return true;
         }
         
