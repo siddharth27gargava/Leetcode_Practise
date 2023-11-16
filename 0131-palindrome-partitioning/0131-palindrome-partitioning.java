6134045cda95763b5a2eeea6e7b5c6df72748e4c
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2023-11-16 17:36:46 -0500

    Time: 8 ms (33.48%), Space: 55.8 MB (11.92%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,39 +1,37 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList();
+        List<List<String>> res = new ArrayList<>();
         
-        generate(0,s,res,new ArrayList());
+        dfs(0,s,res,new ArrayList<>());
         
         return res;
     }
     
-    public void generate(int idx, String s, List<List<String>> res, List<String> subs){
-        if(idx >= s.length()){
-            res.add(new ArrayList(subs));
+    public void dfs(int idx, String s, List<List<String>> res, List<String> curr){
+        //Base Case
+        if(idx == s.length()){
+            res.add(new ArrayList<>(curr));
             return;
         }
         
         for(int j = idx; j < s.length(); j++){
             if(isPalindrome(s,idx,j)){
-                subs.add(s.substring(idx,j+1));
-                generate(j+1,s,res,subs);
-                subs.remove(subs.size() - 1);
+                curr.add(s.substring(idx,j+1));
+                //after figuring out that curr is currently a palindrome, check if there are other palindromes by incrementing the dfs and then clean it up
+                dfs(j+1,s,res,curr);
+                curr.remove(curr.size() - 1);
             }
         }
-        
-        
     }
     
-    public boolean isPalindrome(String s, int i, int j){
-        String ls = s.toLowerCase();
-        int start = i;
-        int end = j;
-        while(start < end){
-            if(ls.charAt(start) != ls.charAt(end)){
+    public boolean isPalindrome(String s, int l, int r){
+        while(l < r){
+            if(s.charAt(l) != s.charAt(r)){
                 return false;
             }
-            start++;
-            end--;
+            
+            l++;
+            r--;
         }
         
         return true;
