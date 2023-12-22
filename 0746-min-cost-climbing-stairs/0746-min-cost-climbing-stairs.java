91d23dab7efbb651684cc6e5931b1f46cb81cff7
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2023-12-21 22:03:40 -0500

    Time: 1 ms (46.05%), Space: 44.2 MB (6.49%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,26 +1,31 @@
 class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int one = minCost(0,cost, dp);
-        int two = minCost(1,cost, dp);
-        
-        return Math.min(one,two);
-    }
     
     int[] dp = new int[1000];
     
-    public int minCost(int idx, int[] cost, int[] dp){
-        if(idx >= cost.length){
+    public int minCostClimbingStairs(int[] cost) {
+        int zero = rec(cost,0);
+        int one = rec(cost,1);
+        
+        return Math.min(zero,one);
+    }
+    
+    public int rec(int[] cost, int curr){
+        
+        int n = cost.length;
+        
+        //Base Case
+        if(curr >= n){
             return 0;
         }
         
-        if(dp[idx] != 0){
-            return dp[idx];
+        if(dp[curr] != 0){
+            return dp[curr];
         }
         
-        int ans = Math.min(minCost(idx + 1,cost,dp),minCost(idx + 2,cost,dp)) + cost[idx];
+        int min = Math.min(rec(cost,curr+1),rec(cost,curr+2)) + cost[curr];
         
-        dp[idx] = ans;
+        dp[curr] = min;
         
-        return ans;
+        return min;
     }
 }
