b458a22eae922feabfef79cfd36f7a92ebc0ac6b
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2023-12-26 00:09:14 -0500

    Time: 1 ms (99.91%), Space: 62.2 MB (6.41%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,20 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        //get the hishest stock arr
-        //calc the max difference
-        int minP = Integer.MAX_VALUE;
-        int totD = Integer.MIN_VALUE;
+        int minPrc = Integer.MAX_VALUE;
+        int maxPrf = Integer.MIN_VALUE;
         
         for(int i = 0; i < prices.length; i++){
-            if(prices[i] < minP){
-                minP = prices[i];
+            if(prices[i] < minPrc){
+                minPrc = prices[i];
             }
             
-            if(prices[i] - minP >= totD){
-                totD = prices[i] - minP;
+            if(prices[i] - minPrc > maxPrf){
+                maxPrf = prices[i] - minPrc;
             }
         }
         
-        return totD;
+        return maxPrf;
     }
 }
