6247169c939839fcd1304b120dcb7aa15f54af9e
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2023-12-26 21:34:22 -0500

    Time: 1 ms (10.09%), Space: 42.5 MB (5.90%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,9 +4,9 @@ class Solution {
     
     public int uniquePaths(int m, int n) {
         
-        if(m == 0 || n == 0){
-            return 0;
-        }
+        // if(m == 0 || n == 0){
+        //     return 0;
+        // }
         
         if(m == 1 || n == 1){
             return 1;
