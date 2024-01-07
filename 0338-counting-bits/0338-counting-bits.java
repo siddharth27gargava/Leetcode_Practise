86ffe31aa0d09575bab8e234d6dbce9f0860100d
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-01-06 22:59:08 -0500

    Time: 6 ms (25.68%), Space: 46.4 MB (65.50%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,16 +1,39 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] dp = new int[n+1];
-        int offset = 1;
+        int[] ans = new int[n+1];
         
-        for(int i = 1; i <= n; i++){
-            if(offset * 2 == i){
-                offset = i;
-            }
-            
-            dp[i] = 1 + dp[i - offset];
+        for(int i = 0; i <= n; i++){
+            ans[i] = countone(i);
         }
         
-        return dp;
+        return ans;
+    }
+    
+    public int countone(int num){
+        int count = 0;
+        while(num > 0){
+            if((num & 1) == 1){
+                count++;
+            }
+            
+            num = num >>> 1;
+        }
+        
+        return count;
+    }
+    
+    public int convertoB(int num){
+        
+        int bnum = 0;
+        int count = 0;
+        
+        while(num > 0){
+            int rem = num % 2;
+            bnum = bnum + ((int)(Math.pow(10,count)) * rem);
+            count++;
+            num = num/2;
+         }
+        
+        return bnum;
     }
 }
