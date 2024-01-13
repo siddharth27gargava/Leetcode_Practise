65434787bc414b629395b5901990ac0e12813098
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-01-13 03:16:33 -0500

    Time: 27 ms (36.33%), Space: 45 MB (28.72%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,35 +1,40 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int lo = 1;
-        int hi = getMax(piles);
-        int ans = hi;
-        
-        while(lo <= hi){
-            int mid = (lo+hi)/2;
+        //for min eating speed, have the ans value as max possible which in this case would be highest value in array
+        int left = 1;
+        int right = getMax(piles);
+        int ans = right;
+
+        while (left <= right) {
+            int mid = (left + right)/2;
             int hours = 0;
-            for(int i = 0; i < piles.length; i++){
+            for (int i = 0; i < piles.length; i++) {
                 hours += (Math.ceil((double)piles[i]/mid));
             }
-            
+
+            //let's say mid is 6, which means hours is 6, but we can further extend hours, which means mid should be smaller
+            //to make mid smaller we reduce right
+
+            //let's say mid is 3, hours is 10, we have to reduce hours, mid should be bigger, to make mid bigger -> left + 1 
             if(hours <= h){
                 ans = Math.min(ans, mid);
-                hi = mid - 1;
+                right = mid - 1;
             } else {
-                lo = mid + 1;
+                left = mid + 1;
             }
         }
-        
+
         return ans;
     }
-    
-    public int getMax(int[] piles){
+
+    public int getMax(int[] arr){
         int max = Integer.MIN_VALUE;
-        for(int i = 0; i < piles.length; i++){
-            if(max < piles[i]){
-                max = piles[i];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
-        
+
         return max;
     }
 }
