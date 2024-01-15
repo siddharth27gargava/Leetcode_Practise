e7763d05c21320b77280328723e9c9ed3deebb4d
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-01-14 22:59:01 -0500

    Time: 122 ms (10.83%), Space: 45.7 MB (7.61%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,81 +1,85 @@
 class Solution {
     public String minWindow(String s, String t) {
-        
+        //Step-0 Initialise ans as empty string
+        //Step-1 create hashmap for mct and fill values
+        //Step-2 put left and right as -1
+        //Step-2.5 initialise mct as 0, and dmct as t.length
+        //Step-3 not a typical while loop instead have while(true)
+        //declare two : flags
+
+        //Step-4 acquire by checking right < s.length-1 and mct < dmct
+        //Increment right, put it in smap
+        //Check if the char is needed in tmap and update mct accordingly
+        //Change the boolean value fo flag to true
+
+        //Step-5 contract the string by checking left < right and mct == dmct
+        //Step-6 get the potential ans as substr(l+1,r+1)
+        //update ans by checking potans length less than ans or ans = 0
+
+        //Step-7  incr left, remove it from smap in two way of -1 or remove
+        //Step-8 subtract value from mct
+        //Change the boolean to true
+        //If both booleans are false, then break
+
         String ans = "";
-        
-        //base case
-        if(s.length() == 0 || t.length() == 0 || t.length() > s.length()){
-            return ans;
+        HashMap<Character,Integer> tMap = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
         }
-        
-        //count for tmap
-        HashMap<Character, Integer> tmap = new HashMap<>();
-        for(int i = 0; i < t.length(); i++){
-            char tcc = t.charAt(i);
-            tmap.put(tcc,tmap.getOrDefault(tcc,0) + 1);
-        }
-        
-        HashMap<Character,Integer> smap = new HashMap<>();
-        
-        //declare mct and dmct
-        int mct = 0;
-        int dmct = t.length();
-        
-        //declare left and right
+
+        HashMap<Character,Integer> sMap = new HashMap<>();
         int left = -1;
         int right = -1;
-        
-        //start with while-true and booleans
-        while(true){
-            boolean floop1 = false;
-            boolean floop2 = false;
-            
-             //acquire with while mct < dmct
-            while(right < s.length()-1 && mct < dmct){
+
+        int mct = 0;
+        int dmct = t.length();
+
+        while (true) {
+            boolean l1 = false;
+            boolean l2 = false;
+
+            while (right < s.length() - 1 && mct < dmct) {
                 right++;
+
                 char scc = s.charAt(right);
-                
-                smap.put(scc,smap.getOrDefault(scc,0) + 1);
-                
-                //increment mct
-                if(smap.getOrDefault(scc,0) <= tmap.getOrDefault(scc,0)){
-                mct++;
+
+                sMap.put(scc, sMap.getOrDefault(scc, 0) + 1);
+
+                if (sMap.getOrDefault(scc, 0) <= tMap.getOrDefault(scc, 0)) {
+                    mct++;
                 }
-                
-                floop1 = true;
+
+                l1 = true;
             }
-            
-            while(left < right && mct == dmct){
-                
-                //calc pot-ans and release from left
-                String potans = s.substring(left+1,right+1);
+
+            while (left < right && mct == dmct) {
+                String potans = s.substring(left + 1, right + 1);
                 if(potans.length() < ans.length() || ans.length() == 0){
                     ans = potans;
                 }
-                
+
                 left++;
+
                 char scc = s.charAt(left);
-                
-                if(smap.get(scc) > 1){
-                    smap.put(scc,smap.get(scc) - 1);
+
+                if(sMap.get(scc) > 1){
+                    sMap.put(scc, sMap.get(scc) - 1);
                 } else {
-                    smap.remove(scc);
+                    sMap.remove(scc);
                 }
-                
-                //remove val from mct
-                if(smap.getOrDefault(scc,0) < tmap.getOrDefault(scc,0)){
+
+                if (sMap.getOrDefault(scc, 0) < tMap.getOrDefault(scc, 0)) {
                     mct--;
                 }
-                
-                floop2 = true;
-                
+
+                l2 = true;
             }
-            
-            if(floop1 == false && floop2 == false){
+
+            if (l1 == false && l2 == false) {
                 break;
             }
         }
-        
+
         return ans;
     }
 }
