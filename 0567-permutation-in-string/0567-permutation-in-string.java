39602746cfb87720e81bb0c193a240acfc44e21b
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-01-14 21:12:31 -0500

    Time: 826 ms (7.78%), Space: 45.3 MB (5.12%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,30 +1,27 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int left = 0;
-        int right = s1.length();
-        
-        HashMap<Character,Integer> maps1 = new HashMap<>();
-        for(int i = 0; i < s1.length(); i++){
-                maps1.put(s1.charAt(i),maps1.getOrDefault(s1.charAt(i),0) + 1);
+        HashMap<Character, Integer> s1map = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            s1map.put(s1.charAt(i), s1map.getOrDefault(s1.charAt(i), 0) + 1);
         }
-        
-        while(right <= s2.length()){
-            
-            HashMap<Character,Integer> maps2 = new HashMap<>();
-            
-            for(int i = left; i < right; i++){
-                maps2.put(s2.charAt(i),maps2.getOrDefault(s2.charAt(i),0) + 1);
+
+        int right = s1.length()-1;
+        int left = 0;
+
+        while (right < s2.length()) {
+            HashMap<Character,Integer> s2map = new HashMap<>();
+            for (int i = left; i <= right; i++) {
+                s2map.put(s2.charAt(i), s2map.getOrDefault(s2.charAt(i), 0) + 1);
             }
-            
-            if(maps1.equals(maps2)){
+
+            if (s1map.equals(s2map)) {
                 return true;
             }
-            
-            right++;
+
             left++;
+            right++;
         }
-        
+
         return false;
     }
-    
 }
