25e6dacd7cfc04a256f432c84ec11b979557f47a
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-01-21 19:37:42 -0500

    Time: 0 ms (100.00%), Space: 42.5 MB (20.37%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,17 +10,24 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        //no need to connect initially prev to curr
         ListNode curr = head;
         ListNode prev = null;
         
-        while(curr!=null){
+        //since we're going to keep on moving till we reach the last node, when curr becomes, the new head is prev since it is behind curr
+        while (curr!=null) {
+            //need this because we have to store the value in-order to shift curr to ahead  
             ListNode ahead = curr.next;
-            
+
+            //switch the direction
             curr.next = prev;
+
+            //update the pointers
             prev = curr;
             curr = ahead;
         }
-        
+
+        //explained 
         return prev;
     }
 }
