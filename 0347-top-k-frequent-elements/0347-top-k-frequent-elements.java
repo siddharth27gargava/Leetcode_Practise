378be11aa432aec53d6ef453bb3aaecda32e3259
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-01-25 17:46:21 -0500

    Time: 16 ms (34.63%), Space: 48.9 MB (16.83%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,28 +1,28 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        //make a min-heap and max-heap
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
 
-        ArrayList<Integer> res = new ArrayList<>();
-        
-        for(int num : nums){
+        for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        for(int num : map.keySet()){
-            pq.add(num);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> (map.get(a) - map.get(b)));
 
-            while (pq.size() > k) {//when doing while it should be opposite of what you do for if
+        for (int num : map.keySet()) {
+            pq.add(num);
+            if (pq.size() > k) {
                 pq.poll();
             }
+            
         }
 
-        while(!pq.isEmpty()){
+        int[] ans = new int[pq.size()];
+
+        ArrayList<Integer> res = new ArrayList<>();
+
+        while (!pq.isEmpty()) {
             res.add(pq.poll());
         }
-
-        int[] ans = new int[k];
 
         for (int i = 0; i < ans.length; i++) {
             ans[i] = res.get(i);
