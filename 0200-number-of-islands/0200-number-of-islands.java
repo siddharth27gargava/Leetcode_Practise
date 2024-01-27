a4db90b6aec90d9d645f3f7aff0b491c198e61ca
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-01-27 16:53:12 -0500

    Time: 3 ms (84.52%), Space: 49.9 MB (25.58%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,6 +1,6 @@
 class Solution {
 
-    boolean[][] visited;
+    //boolean[][] visited;
 
     public int numIslands(char[][] grid) {
         //standard step
@@ -8,15 +8,15 @@ class Solution {
         int cols = grid[0].length;
         int count = 0;
 
-        visited = new boolean[grid.length][grid[0].length];
+        //visited = new boolean[grid.length][grid[0].length];
 
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
                 //if not visited and matrix value is 1, we can start search for an island and increment count
-                if (grid[r][c] == '1' && !visited[r][c]) {
+                if (grid[r][c] == '1') {
                     count++;
                     //traverse completely all the connected ones since we want to increment only once for all banded ones
-                    dfs(r, c, rows, cols, grid, visited);
+                    dfs(r, c, rows, cols, grid);
                 }
             }
         }
@@ -24,17 +24,17 @@ class Solution {
         return count;
     }
 
-    public void dfs(int r, int c, int rows, int cols, char[][] grid, boolean[][] visited){
-        if (r < 0 || r > rows-1 || c < 0 || c > cols-1 || grid[r][c] == '0' || visited[r][c]) {
+    public void dfs(int r, int c, int rows, int cols, char[][] grid){
+        if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] == '0') {
             return;
         }
 
-        visited[r][c] = true;
+        grid[r][c] = '0';
 
-        dfs(r+1, c, rows, cols, grid, visited);
-        dfs(r-1, c, rows, cols, grid, visited);
-        dfs(r, c+1, rows, cols, grid, visited);
-        dfs(r, c-1, rows, cols, grid, visited);
+        dfs(r+1, c, rows, cols, grid);
+        dfs(r-1, c, rows, cols, grid);
+        dfs(r, c+1, rows, cols, grid);
+        dfs(r, c-1, rows, cols, grid);
 
     }
 }
