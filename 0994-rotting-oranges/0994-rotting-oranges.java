def45436781c13c5b56142c57b9f0e4086e1ad3e
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-01-27 22:40:28 -0500

    Time: 2 ms (79.54%), Space: 42.8 MB (17.13%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,60 +1,53 @@
-class Solution {
-    
-    public class Pair{
+
+import java.awt.GridLayout;class Solution {
+    public class Pair {
         int r;
         int c;
-        
+
         public Pair(int r, int c){
             this.r = r;
             this.c = c;
         }
     }
-    
-    public final int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
+
+    public int[][] twod = {{0,1},{0,-1},{1,0},{-1,0}};
     
     public int orangesRotting(int[][] grid) {
-        //Identified with BFS
-        //1. Pair
-        //Done
-        
-        //create q;
-        
-        Deque<Pair> q = new LinkedList<>();
-        int time = 0;
-        int fresh = 0;
         int rows = grid.length;
         int cols = grid[0].length;
+
+        int fresh = 0;
+        int time = 0;
+
+        Deque<Pair> q = new LinkedList<>();
         
-        //get all fresh orange count
-        for(int r = 0; r < rows; r++){
-            for(int c = 0; c < cols; c++){
-                if(grid[r][c] == 1){
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
                     fresh++;
                 }
-                
-                if(grid[r][c] == 2){
-                    q.addLast(new Pair(r,c));
+
+                if (grid[i][j] == 2) {
+                    q.addLast(new Pair(i,j));
                 }
             }
         }
-        
-        //get all rotten oranges in pair
-        //Done
-        
-        while(!q.isEmpty() && fresh > 0){
+
+        //bfs starts
+        while (!q.isEmpty() && fresh > 0) {
             int qs = q.size();
-            for(int i = 0; i < qs; i++){
+            for (int i = 0; i < qs; i++) {
                 Pair rmv = q.removeFirst();
-                
-                for(int j = 0; j < directions.length; j++){
-                    Pair np = new Pair(0,0);
-                    np.r = rmv.r + directions[j][0];
-                    np.c = rmv.c + directions[j][1];
-                    
-                    if(np.r < 0 || np.r >= rows || np.c < 0 || np.c >= cols || grid[np.r][np.c] != 1){
+                //new pair to be formed inside the direction loop
+                for (int j = 0; j < twod.length; j++) {
+                    Pair np = new Pair(0, 0);
+                    np.r = rmv.r + twod[j][0];
+                    np.c = rmv.c + twod[j][1];
+
+                    if (np.r < 0 || np.r >= rows || np.c < 0 || np.c >= cols || grid[np.r][np.c] != 1) {
                         continue;
                     }
-                    
+
                     grid[np.r][np.c] = 2;
                     q.addLast(np);
                     fresh--;
@@ -62,20 +55,11 @@ class Solution {
             }
             time++;
         }
-        
-        if(fresh>0){
+
+        if (fresh > 0) {
             return -1;
-        } else {
-            return time;
         }
-        
-        //BFS--
-        //go through each first pair--
-        //add it to your array--
-        //check if it's not fresh or in limit, then continue
-        //otherwise - add that in q and fresh--;
-        //add time for each loop
-        //return time
-        
+
+        return time;
     }
 }
