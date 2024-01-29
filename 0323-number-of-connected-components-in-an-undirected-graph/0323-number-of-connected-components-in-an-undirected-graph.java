7986944fea299c2f93311ef9e3560f70d2654e77
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-01-29 11:55:05 -0500

    Time: 8 ms (27.9%), Space: 45.6 MB (12.22%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,37 +1,40 @@
 class Solution {
     public int countComponents(int n, int[][] edges) {
-        //Creating an adj. list in undirected graph
-        HashMap<Integer,List<Integer>> adMap = new HashMap<>();
-        
-        for(int i = 0; i < n; i++){
-            adMap.put(i, new ArrayList<>());
-        }
-        
-        for(int[] edgeMember: edges){
-            adMap.get(edgeMember[0]).add(edgeMember[1]);
-            adMap.get(edgeMember[1]).add(edgeMember[0]);
-        }
-        
-        HashSet<Integer> visit = new HashSet<>();
         
         int count = 0;
         
-        for(int i = 0; i < n; i++){
-            if(!visit.contains(i)){
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(i, new ArrayList<>());
+        }
+
+        //adj list undirected
+        for (int[] edge : edges) {
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);
+        }
+
+        //list done
+
+        HashSet<Integer> visit = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!visit.contains(i)) {
                 visit.add(i);
                 count++;
-                dfs(i,adMap,visit);
+                dfs(i, map, visit);
             }
         }
-        
+
         return count;
     }
-    
-    public void dfs(int i, HashMap<Integer,List<Integer>> adMap, HashSet<Integer> visit){
-        for(int j : adMap.get(i)){
-            if(!visit.contains(j)){
-                visit.add(j);
-                dfs(j,adMap,visit);
+
+    public void dfs(int i, HashMap<Integer, List<Integer>> map, HashSet<Integer> visit){
+        for (int nbr : map.get(i)) {
+            if(!visit.contains(nbr)){
+                visit.add(nbr);
+                dfs(nbr, map, visit);
             }
         }
     }
