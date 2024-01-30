7da4a4d67675191017f941e8c02a3bd27a37b83e
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-01-29 20:24:40 -0500

    Time: 2 ms (96.84%), Space: 44.6 MB (13.26%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,31 +15,35 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(subRoot == null){
+        
+        if (subRoot == null) {
             return true;
         }
         
-        if(root == null){
+        if (root == null) {
             return false;
         }
-        
-        //check if they are same tree
-        if(isSameTree(root,subRoot)){
+
+        if (isSameTree(root,subRoot)) {
             return true;
         }
-        
-        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot); 
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
-    
+
     public boolean isSameTree(TreeNode p, TreeNode q){
-        if(p == null && q == null){
+        if (p == null && q == null) {
             return true;
         }
-        
-        if(p!=null && q!=null && p.val==q.val){
-            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+
+        if (p == null || q == null) {
+            return false;
         }
-        
+
+        if(p.val == q.val){
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+
         return false;
     }
 }
