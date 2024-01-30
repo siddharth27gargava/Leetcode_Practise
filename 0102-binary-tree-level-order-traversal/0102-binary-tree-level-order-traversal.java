df0980216925f1330b3f1b70e7960cba697e8931
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-01-29 20:49:17 -0500

    Time: 2 ms (23.48%), Space: 44.9 MB (45.96%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,3 +1,7 @@
+
+
+import com.sun.source.tree.Tree;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,35 +19,34 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        
-        List<List<Integer>> res = new ArrayList<>();
-        
-        if(root == null){
-            return res;
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
         }
-        
         Deque<TreeNode> q = new LinkedList<>();
-        
+
         q.addLast(root);
+
         int level = 0;
-        
-        while(!q.isEmpty()){
-            int qs = q.size();
-            res.add(new ArrayList<>());
-            for(int i = 0; i < qs; i++){
-                TreeNode qans = q.removeFirst();
-                res.get(level).add(qans.val);
-                if(qans.left!=null){
-                    q.addLast(qans.left);
+
+        while (!q.isEmpty()) {
+            int qs = q.size(); //reason why we run loop at this is because we want to process only those elements which are in the q at this time
+            ans.add(new ArrayList<>());
+            for (int i = 0; i < qs; i++) {
+                TreeNode rmv = q.removeFirst();
+                //very improtant that you forget
+                ans.get(level).add(rmv.val);
+
+                if (rmv.left != null) {
+                    q.addLast(rmv.left);
                 }
-                
-                if(qans.right!=null){
-                    q.addLast(qans.right);
+                if (rmv.right != null) {
+                    q.addLast(rmv.right);
                 }
             }
             level++;
         }
-        
-        return res;
+
+        return ans;
     }
 }
