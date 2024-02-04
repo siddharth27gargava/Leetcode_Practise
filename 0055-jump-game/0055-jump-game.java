046bea7a3176b45ef4b3903f1736bdddee56cae7
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-02-04 10:13:31 -0500

    Time: 1 ms (99.97%), Space: 45.2 MB (73.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        //goal where we want to reach
-        int goal = nums[nums.length - 1];
-        for(int i = nums.length - 1; i >= 0; i--){
+        int goal = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
             
-            //check at every instance if we can reach goal from i, then bring goal closer
-            if(i + nums[i] >= goal){
+            if (nums[i] + i >= goal) {
                 goal = i;
             }
         }
-        
-        if(goal > 0){
+
+        if (goal > 0) {
             return false;
         }
-        
+
         return true;
     }
 }
