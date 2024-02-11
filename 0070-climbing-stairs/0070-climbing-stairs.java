4d66ede268fd07bcedc177b96f9633a1a44ffc7f
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-02-10 19:42:00 -0500

    Time: 0 ms (100%), Space: 40.4 MB (30.74%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,38 +1,27 @@
 class Solution {
-    
-    public int[] dp = new int[100];
-    
+
+    int[] dp = new int[50];
     public int climbStairs(int n) {
-        //do it with recursion
-        if(n == 0){
-            return 0;
-        }
-        
-        if(n == 1){
+        return recursion(0, n);
+    }
+
+    public int recursion(int curr, int n){
+        if (curr == n) {
             return 1;
         }
-        
-        return rec(0,n);
-        
-    }
-    
-    public int rec(int curr, int n){
-        if(curr > n){
+
+        if (curr > n) {
             return 0;
         }
-        
-        if(dp[curr] != 0){
+
+        if (dp[curr] != 0) {
             return dp[curr];
         }
-        
-        if(curr == n){
-            return 1;
-        }
-        
-        int ans = rec(curr + 1, n) + rec(curr + 2,n);
-        
+
+        int ans = recursion(curr + 1, n) + recursion(curr + 2, n);
+
         dp[curr] = ans;
-        
-        return ans;
+
+        return ans; 
     }
 }
