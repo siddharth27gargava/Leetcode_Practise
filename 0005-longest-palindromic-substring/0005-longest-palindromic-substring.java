b8ecb6f48f4f959046e60876023b783b74a22ec6
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-02-10 20:36:03 -0500

    Time: 2649 ms (5%), Space: 45.4 MB (29.84%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,35 +1,37 @@
 class Solution {
     public String longestPalindrome(String s) {
         
-        String longest = "";
-        for(int i = 0; i < s.length(); i++){
-            for(int j = i+1; j <= s.length(); j++){
-                if(isPalindrome(s.substring(i,j))){
-                    String nstr = s.substring(i,j);
-                    
-                    if(nstr.length() > longest.length()){
-                        longest = nstr;
+        String ans = "";
+        int maxl = ans.length();
+        
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                if (isPalindrome(s.substring(i,j))) {
+                    if (s.substring(i, j).length() > maxl) {
+                        maxl = s.substring(i, j).length();
+                        ans = s.substring(i, j);
                     }
                 }
             }
         }
-        
-        return longest;
+
+        return ans;
     }
-    
-    public boolean isPalindrome(String word){
+
+    public boolean isPalindrome(String s){
         int left = 0;
-        int rt = word.length() - 1;
-        
-        while(left < rt){
-            if(word.charAt(left) != word.charAt(rt)){
+        int right = s.length() - 1;
+
+        //do this because we also want to check the edge case of that mid element
+        while (left <= right) {
+            if (s.charAt(left) != s.charAt(right)) {
                 return false;
             }
-            
+
             left++;
-            rt--;
+            right--;
         }
-        
+
         return true;
     }
 }
