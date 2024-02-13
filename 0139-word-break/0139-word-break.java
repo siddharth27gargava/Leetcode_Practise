3cac1dbf7a1cd8a85efbdbeda1585e9302cf4050
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-02-12 23:17:59 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,28 +1,26 @@
 class Solution {
-    
-    HashMap<String, Boolean> dp = new HashMap<>();;
-    
+
+    HashMap<String, Boolean> dp;
+
     public boolean wordBreak(String s, List<String> wordDict) {
         
-        
-        
+        dp = new HashMap<>();
         if(wordDict.contains(s)){
             return true;
         }
-        
-        if(dp.containsKey(s)){
+
+        if (dp.containsKey(s)) {
             return dp.get(s);
         }
-        
-        for(int i = 1; i <= s.length(); i++){
-            String left = s.substring(0,i);
-            if(wordDict.contains(left) && wordBreak(s.substring(i),wordDict)){
-                dp.put(s,true);
+
+        for (int i = 1; i <= s.length(); i++) {
+            if (wordDict.contains(s.substring(0,i)) && wordBreak(s.substring(i), wordDict)) {
+                dp.put(s, true);
                 return true;
             }
         }
-        
-        dp.put(s,false);
+
+        dp.put(s, false);
         return false;
     }
 }
