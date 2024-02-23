4cd791da9abd391d083fe0f2097b38923658c6d5
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-02-22 23:37:03 -0500

    Time: 34 ms (64.6%), Space: 45.3 MB (79.72%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -1,38 +1,40 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        if(hand.length % groupSize != 0){
+        //think of it as - pq which stores the lowest values ascending order numbers and 
+        //hashmap which we use to track freq of each numbers
+
+        if (hand.length % groupSize != 0) {
             return false;
         }
-        
-        HashMap<Integer,Integer> map = new HashMap<>();
-        
-        for(int h : hand){
-            map.put(h,map.getOrDefault(h,0) + 1);
+
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int num : hand) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
-        
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-        
-        //Find min value in logn times
-        for(int h : hand){
-            q.add(h);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int num : hand) {
+            pq.add(num);
         }
-        
-        while(!q.isEmpty()){
-            int min_val = q.poll();
-            
-            if(map.get(min_val) == 0){
+
+        while (!pq.isEmpty()) {
+            int min_val = pq.poll();
+
+            //this means we have already visited this 
+            if (freq.get(min_val) == 0) {
                 continue;
             }
-            
-            for(int i = min_val; i < min_val + groupSize; i++){
-                if(!map.containsKey(i) || map.get(i) == 0){
+
+            for (int i = min_val; i < min_val + groupSize; i++) {
+                if (!freq.containsKey(i) || freq.get(i) == 0) {
                     return false;
                 }
-                
-                map.put(i,map.get(i) - 1);
+
+                freq.put(i, freq.get(i) - 1);
             }
         }
-        
-        return true;
+
+        return true;   
     }
 }
