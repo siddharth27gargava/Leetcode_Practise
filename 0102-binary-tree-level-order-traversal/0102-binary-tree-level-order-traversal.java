dbd2412ba0eaa3fe47158449ed1a187ceb58b7f8
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-03-13 01:44:45 -0400

    Time: 1 ms (88.41%), Space: 45.1 MB (13.70%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,7 +1,3 @@
-
-
-import com.sun.source.tree.Tree;
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -20,26 +16,28 @@ import com.sun.source.tree.Tree;
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
-        if (root == null) {
+
+        Deque<TreeNode> q = new LinkedList<>(); 
+        
+        if(root == null){
             return ans;
         }
-        Deque<TreeNode> q = new LinkedList<>();
 
         q.addLast(root);
-
         int level = 0;
 
         while (!q.isEmpty()) {
-            int qs = q.size(); //reason why we run loop at this is because we want to process only those elements which are in the q at this time
+            int qs = q.size();
             ans.add(new ArrayList<>());
             for (int i = 0; i < qs; i++) {
                 TreeNode rmv = q.removeFirst();
-                //very improtant that you forget
+
                 ans.get(level).add(rmv.val);
 
                 if (rmv.left != null) {
                     q.addLast(rmv.left);
                 }
+
                 if (rmv.right != null) {
                     q.addLast(rmv.right);
                 }
