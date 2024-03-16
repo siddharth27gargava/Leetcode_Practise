a6690b97bce1bf5bd1a06bf1ffbce4615bb17b9b
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-03-15 21:46:09 -0400

    Time: 30 ms (75.93%), Space: 51.2 MB (78.15%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        
         List<List<Integer>> ans = new ArrayList<>();
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            int left = i+1;
-            int right = nums.length - 1;
-            int target = 0 - nums[i];
-            if (i == 0 || i > 0 && (nums[i] != nums[i-1])) {
-                while (left < right) {
-                    if (nums[left] + nums[right] == target) {
-                        ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+        Arrays.sort(nums);
 
-                        while (left < right && nums[left]==nums[left+1]) {
+        //-1,-1,0,0,1,2,-1,4
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            //constraint of not including duplicate
+            if (i == 0 || (nums[i-1] != nums[i])) {
+                int left = i+1;
+                int right = nums.length - 1;
+                int target = 0 - nums[i];
+                while (left < right) {
+                    if(nums[left] + nums[right] == target){
+                        ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        //skip duplicates
+                        while ((left < right) && nums[left] == nums[left+1]) {
                             left++;
                         }
 
-                        while (left < right && nums[right]==nums[right-1]) {
+                        while ((left < right) && nums[right] == nums[right - 1]) {
                             right--;
                         }
+
                         left++;
                         right--;
-                    } else if(nums[left] + nums[right] > target){
-                        right--;
+                    } else if(nums[left] + nums[right] < target){
+                        left++;
                     } else {
-                        left++;
+                        right--;
                     }
                 }
+                // ---3------**7**-------10--------16
             }
         }
 
