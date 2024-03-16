0081b8295436515c27db4bf3f21b118aee075f29
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-03-15 21:35:18 -0400

    Time: 4 ms (57.1%), Space: 43.1 MB (59.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,30 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String ls = s.toLowerCase();
-        String qs = remove(ls);
-        
+        String rem = s.trim();
+        String remlow = rem.toLowerCase();
+        String onlyletters = removeSpecial(remlow);
+        //palap
         int left = 0;
-        int right = qs.length() - 1;
+        int right = onlyletters.length() - 1;
         while(left <= right){
-            if(qs.charAt(left) != qs.charAt(right)){
+            if (onlyletters.charAt(left) != onlyletters.charAt(right)) {
                 return false;
             }
-            
             left++;
             right--;
         }
-        
+
         return true;
     }
-    
-    public String remove(String s){
+
+    public String removeSpecial(String s){
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
-            if((s.charAt(i) >= 'a' && s.charAt(i) <= 'z') || (s.charAt(i)>='0'&&s.charAt(i)<='9')){
+        for (int i = 0; i < s.length(); i++) {
+            if ((s.charAt(i) >= 'a' && s.charAt(i) <= 'z') || (s.charAt(i) >= '0' && s.charAt(i) <= '9') ) {
                 sb.append(s.charAt(i));
             }
         }
-        
+
         return sb.toString();
     }
 }
