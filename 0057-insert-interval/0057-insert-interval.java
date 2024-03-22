d072fefc04084aee55f7217706a731cb33407488
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-03-21 22:01:37 -0400

    Time: 1 ms (98.57%), Space: 44.9 MB (37.86%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,36 +1,31 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        
-        //1) Create a result list
-        List<int[]> res = new ArrayList<>();
-        
+        //List of Array
+        List<int[]> ans = new ArrayList<>();
+
         int i = 0;
         int n = intervals.length;
-        
-        //2) When new interval is totally greater than previous intervals
+
+        //-----    iv[i][0]-----------     ----
+        //      ------------- ni[1]
         while(i < n && intervals[i][1] < newInterval[0]){
-            res.add(intervals[i]);
+            ans.add(intervals[i]);
             i++;
         }
-        
-        //3) when new interval overlaps which means new interval end is greater than prev interval start
+
         while(i < n && intervals[i][0] <= newInterval[1]){
             newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
             newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
             i++;
         }
-        
-        //4) after modifying the new interval, add it
-        res.add(newInterval);
-        
-        //5) add the remaining intervals left
+
+        ans.add(newInterval);
+
         while(i < n){
-            res.add(intervals[i]);
+            ans.add(intervals[i]);
             i++;
         }
-        
-        return res.toArray(new int[res.size()][2]);
-        
-        
+
+        return ans.toArray(new int[ans.size()][2]);
     }
 }
