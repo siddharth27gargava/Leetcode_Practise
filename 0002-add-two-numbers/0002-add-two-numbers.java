26b6ed6c254d7b28364c515fd90d4bbfd0af4fb1
siddharth27gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-03-23 13:08:46 -0400

    Time: 1 ms (100%), Space: 44.8 MB (14.49%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,64 +1,55 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // ListNode dummy = new ListNode();
-        // dummy.next = head;
-        // ListNode second = dummy;
-        
         ListNode dummy = new ListNode();
         ListNode curr = dummy;
-        int carry = 0; 
-        int v1 = 0; 
+        int v1 = 0;
         int v2 = 0;
-        
-        while(l1 != null || l2 != null || carry != 0){
-            
-            //Get l1 val
-            if(l1!=null){
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            if (l1 != null) {
                 v1 = l1.val;
-            } else{
+            } else {
                 v1 = 0;
             }
-            
-            //Get v2 val
-            if(l2!=null){
+
+            if (l2 != null) {
                 v2 = l2.val;
-            } else{
+            } else {
                 v2 = 0;
             }
-            
-            //Add
-            int value = (v1+v2+carry);
-            carry = value/10;
-            value = value%10;
-            
-            curr.next = new ListNode(value);
+            int add = v1 + v2 + carry;
+            carry = add / 10;
+            add = add % 10;
+
+            ListNode nn = new ListNode(add);
+            curr.next = nn;
+
             curr = curr.next;
-            
-            if(l1!=null){
+
+            if (l1 != null) {
                 l1 = l1.next;
-            } else{
+            } else {
                 l1 = null;
             }
-            
-            if(l2!=null){
+
+            if (l2 != null) {
                 l2 = l2.next;
-            } else{
+            } else {
                 l2 = null;
             }
-            
-            
         }
-        
+
         return dummy.next;
     }
 }
