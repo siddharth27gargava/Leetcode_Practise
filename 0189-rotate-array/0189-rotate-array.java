9658c77228d560ea8e78c2dafe0ab52ea6355fbb
Siddharth Gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-04-23 19:29:22 -0400

    Time: 2 ms (31.74%), Space: 57.3 MB (55.42%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,22 +1,19 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-       
-       int n = nums.length;
-       int i = 0;
-       
-       //4,5,1,2,3
-       int[] res = new int[nums.length];
+        int n = nums.length;
+        k = k % n;
+        int i = 0;
+        // 1,2,3,4,5,6 -> 4,5,6,1,2,3
+        int[] res = new int[nums.length];
+        // i + k %n position par you'll have ith value
+        while (i < n) {
+            int places = (i + k) % n;
+            res[places] = nums[i];
+            i++;
+        }
 
-       // use concept of places : (i+k) % n and in index value, put ith value
-
-       while (i < res.length) {
-           int places = (i+k) % n;
-           res[places] = nums[i];
-           i++;
-       }
-
-       for (int j = 0; j < res.length; j++) {
-           nums[j] = res[j];
-       }
+        for (int j = 0; j < res.length; j++) {
+            nums[j] = res[j];
+        }
     }
 }
