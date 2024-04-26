096526293e315bc841080929a654ebb041e6bf25
Siddharth Gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-04-25 21:11:00 -0400

    Time: 1 ms (60.51%), Space: 42.8 MB (28.34%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,27 +1,24 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        
+
         Arrays.sort(nums);
-        
-        generate(0,nums,ans,new ArrayList<Integer>());
-        
+
+        helper(0, nums, new ArrayList<>(), ans);
+
         return ans;
     }
-    
-    public void generate(int idx, int[] nums, List<List<Integer>> result, List<Integer> curr){
-        //Base Case when idx reaches till length of nums
+
+    public void helper(int idx, int[] nums, List<Integer> curr, List<List<Integer>> ans){
         if(idx == nums.length){
-            result.add(new ArrayList<>(curr));
+            ans.add(new ArrayList<>(curr));
             return;
         }
-        
-        //Include the idx in subset
+
         curr.add(nums[idx]);
-        generate(idx+1,nums,result,curr);
-        
-        //Don't include
+        helper(idx+1,nums,curr,ans);
+
         curr.remove(curr.size() - 1);
-        generate(idx + 1,nums,result,curr);
+        helper(idx + 1, nums, curr, ans);
     }
 }
