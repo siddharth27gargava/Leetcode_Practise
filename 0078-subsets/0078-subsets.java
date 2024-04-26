36f6dc2db42a27d511e3b1a90ba90f5b7528757a
Siddharth Gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-04-25 21:13:17 -0400

    Time: 1 ms (60.51%), Space: 42.8 MB (28.34%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,24 +1,29 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
 
         Arrays.sort(nums);
 
-        helper(0, nums, new ArrayList<>(), ans);
+        recursion(res, new ArrayList<>(), 0, nums);
 
-        return ans;
+        return res;
     }
 
-    public void helper(int idx, int[] nums, List<Integer> curr, List<List<Integer>> ans){
-        if(idx == nums.length){
-            ans.add(new ArrayList<>(curr));
+    //O(n) - time
+    //O(n) - space
+
+    public void recursion(List<List<Integer>> res, List<Integer> curr, int idx, int[] nums){
+        if (idx == nums.length) {
+            res.add(new ArrayList<>(curr));
             return;
         }
 
+        //include
         curr.add(nums[idx]);
-        helper(idx+1,nums,curr,ans);
+        recursion(res, curr, idx + 1, nums);
 
+        //exclude
         curr.remove(curr.size() - 1);
-        helper(idx + 1, nums, curr, ans);
+        recursion(res, curr, idx + 1, nums);
     }
 }
