4da29ad7b85753b8f97a570ef22142f15ca230da
Siddharth Gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-05-09 14:35:10 -0400

    Time: 48 ms (15.91%), Space: 55.1 MB (64.7%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,97 +1,101 @@
 class Trie {
-    
+
     class Node{
         char data;
         boolean isTerminal;
         HashMap<Character, Node> children;
-        
-        Node(char data, boolean isTerminal){
+
+        public Node(char data, boolean isTerminal){
             this.data = data;
             this.isTerminal = isTerminal;
             this.children = new HashMap<>();
         }
     }
-    
+
     Node root;
     int numWords;
-    
+
     public Trie() {
-        this.root = new Node('\0',false);
+        this.root = new Node('\0', false);
         this.numWords = 0;
     }
     
     public void insert(String word) {
-        this.insert(this.root,word);
+        this.insert(this.root, word);
     }
-    
+
     private void insert(Node parent, String word){
-        
         //BCase
+
+        //have we reached the end of the word
         if(word.length() == 0){
+            //if the isterminal property is not false
             if(!parent.isTerminal){
                 parent.isTerminal = true;
                 this.numWords++;
             }
             return;
         }
-        
-        //SWork
+
+        //Swork
         char cc = word.charAt(0);
         String ros = word.substring(1);
+        //adding that character in child node
         Node child = parent.children.get(cc);
-        if(child == null){
+        if (child == null) {
             child = new Node(cc,false);
-            parent.children.put(cc,child);
+            parent.children.put(cc, child);
         }
-        
-        //RWork
-        this.insert(child,ros);
+
+        //recursion work
+        //child becomes parent and ros is the word
+        this.insert(child, ros);
     }
     
     public boolean search(String word) {
-        return this.search(this.root,word);
+        return this.search(root, word);
     }
-    
+
     private boolean search(Node parent, String word){
-         //BCase
-        if(word.length() == 0){
-            if(parent.isTerminal){
+        //base case
+        if (word.length() == 0) {
+            if (parent.isTerminal) {
                 return true;
             } else {
                 return false;
             }
         }
-        
-        //Swork
+
+        //short work
         char cc = word.charAt(0);
         String ros = word.substring(1);
         Node child = parent.children.get(cc);
-        if(child == null){
+        if (child == null) {
             return false;
         }
-        
-        return this.search(child,ros);
+
+        //rec-case
+        return this.search(child, ros);
     }
     
     public boolean startsWith(String prefix) {
-        return this.startsWith(this.root,prefix);
+        return this.startsWith(root, prefix);
     }
-    
+
     private boolean startsWith(Node parent, String prefix){
-         //BCase
-        if(prefix.length() == 0){
-           return true;
+        //base case
+        if (prefix.length() == 0) {
+            return true;
         }
-        
-        //Swork
+
         char cc = prefix.charAt(0);
         String ros = prefix.substring(1);
         Node child = parent.children.get(cc);
-        if(child == null){
+        if (child == null) {
             return false;
         }
-        
-        return this.startsWith(child,ros);
+
+        return this.startsWith(child, ros);
     }
 }
 
