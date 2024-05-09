cb135d1a6aea6cff6ae691e6b668ee44eeb7d6d7
Siddharth Gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-05-08 23:21:16 -0400

    Time: 14 ms (35.99%), Space: 46.1 MB (15.91%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,27 +1,39 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-         //create a char map
-        int[] char_map = new int[26];
+        //thought process is to first fit the max occurring tasks and then the remaining one
+        HashMap<Character, Integer> fmap = new HashMap<>();
+        //Approach
+        //1. after figuring out max tasks, and finding idle slots - by (x - 1)*(n+1)
+        //2. deterrmining the number of max frequency tasks - they can all be placed in the final unit of the 
+        //cooldown period of the max frequency task without violating the cooldown requirement
+        //3. calc total time : Max of either the total idle time OR total task list length
 
-        for (char c : tasks){
-            char_map[c - 'A']++;
+        for(char cc : tasks){
+            fmap.put(cc, fmap.getOrDefault(cc, 0) + 1);
         }
 
-        Arrays.sort(char_map);
-        int max_value = char_map[25] - 1;
-        int idle_slots = max_value * n;
+        int maxFreq = 0;
+        char maxElement;
 
-        for (int i = 24; i >= 0 ; i--) {
-            idle_slots -= Math.min(max_value,char_map[i]);
-            //we do math.min with max_value because if there is similar frequency
-            //of another char, then idle slot will be filled only till max_val and that
-            //task will come after max_value
+        for(Map.Entry<Character, Integer> entry: fmap.entrySet()){
+            if (entry.getValue() > maxFreq) {
+                maxFreq = entry.getValue();
+                maxElement = entry.getKey();
+            }
         }
 
-        if (idle_slots > 0){
-            return idle_slots + tasks.length;
+        int idle_time = (maxFreq - 1) * (n + 1);
+
+        int count_of_max_elements = 0;
+
+        for(char cc : fmap.keySet()){
+            if (fmap.get(cc) == maxFreq) {
+                count_of_max_elements++;
+            }
         }
-        
-        return tasks.length;
+
+        int ans = Math.max(idle_time + count_of_max_elements, tasks.length);
+
+        return ans;
     }
 }
