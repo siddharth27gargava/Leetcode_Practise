03d6e22750f57419fdc4ebb444dba613d3918d55
Siddharth Gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-05-09 17:25:27 -0400

    Time: 39 ms (97.16%), Space: 111 MB (94.15%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,79 +1,82 @@
 class LRUCache {
-    
     public class Node{
         int key;
-        int value;
+        int val;
+        //double-ended linkedlist
         Node next;
-        Node prev;  
+        Node prev;
+
+        public Node(){};
+
+        public Node(int key, int val){
+            this.key = key;
+            this.val = val;
+        }
     }
-    
+
     HashMap<Integer, Node> map;
     Node head = new Node();
-    Node tail = new Node();
+    Node tail = new Node();  
     int capacity = 0;
-    
+
     public LRUCache(int capacity) {
         map = new HashMap<>();
-        
-        //double lineked list
+        this.capacity = capacity;
+
+        //connect double-ended linkedlist
         head.next = tail;
         tail.prev = head;
-        
-        this.capacity = capacity;
     }
-    
+
     public void add(Node node){
         Node head_next = head.next;
-        
-        //Connect node with head_next
+
+        //connect node with head_next
         node.next = head_next;
         head_next.prev = node;
-        
-        //Connect node with head
+
+        //connect node with head
         head.next = node;
         node.prev = head;
     }
-    
+
     public void remove(Node node){
         Node node_prev = node.prev;
         Node node_next = node.next;
-        
-        //Connect node_prev and node_next
+
         node_prev.next = node_next;
         node_next.prev = node_prev;
     }
     
     public int get(int key) {
         int result = -1;
-        
+
         Node nn = map.get(key);
-        
-        if(nn!=null){
+
+        if (nn!=null) {
             remove(nn);
-            result = nn.value;
+            result = nn.val;
             add(nn);
         }
-        
+
         return result;
     }
     
     public void put(int key, int value) {
         Node nn = map.get(key);
-        
-        if(nn!=null){
+
+        if (nn != null) {
             remove(nn);
-            nn.value = value;
+            nn.val = value;
             add(nn);
-        } else{
+        } else {
             if(map.size() >= capacity){
                 map.remove(tail.prev.key);
                 remove(tail.prev);
-            } 
-            
-            Node node = new Node();
-            node.value = value;
-            node.key = key;
-            map.put(key,node);
+            }
+
+            Node node = new Node(key,value);
+            map.put(key, node);
             add(node);
         }
     }
