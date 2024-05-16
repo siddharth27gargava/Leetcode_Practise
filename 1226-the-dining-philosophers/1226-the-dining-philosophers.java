ebfb36a245cc3b122b3f2d8281e093e6245be8bf
Siddharth Gargava <42087729+siddharth27gargava@users.noreply.github.com>
2024-05-16 15:25:41 -0400

    Time: 14 ms (47.63%), Space: 45.4 MB (61.84%) - LeetHub

---
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.java
@@ -0,0 +1,24 @@
+class DiningPhilosophers {
+    Semaphore s = new Semaphore(1);
+
+    public DiningPhilosophers() {
+        
+    }
+
+    // call the run() method of any runnable to execute its code
+    public void wantsToEat(int philosopher,
+                           Runnable pickLeftFork,
+                           Runnable pickRightFork,
+                           Runnable eat,
+                           Runnable putLeftFork,
+                           Runnable putRightFork) throws InterruptedException {
+        
+        s.acquire();
+        pickLeftFork.run();
+        pickRightFork.run();
+        eat.run();
+        putLeftFork.run();
+        putRightFork.run();
+        s.release();
+    }
+}
